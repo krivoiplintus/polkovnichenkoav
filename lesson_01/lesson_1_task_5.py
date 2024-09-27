ac4b359b1d549d195fc43f71ece606ad58fb2167
Teem Teem <krivoi.plintus@yandex.ru>
2024-09-27 11:41:26 +0300

    first commit

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def printNumber(num):
+    print(num, end='')
+
+
+printNumber(8)
+printNumber(8)
+printNumber(0)
+printNumber(0)
+printNumber(5)
+printNumber(5)
+printNumber(5)
+printNumber(3)
+printNumber(5)
+printNumber(3)
+printNumber(5)
